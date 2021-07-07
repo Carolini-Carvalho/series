ac345a313f0de2c4606f7b17917fd17230b8fecf
carolini <acarolini@icloud.com>
2021-07-07 12:58:18 -0300

    relacionando models

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Serie;
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
