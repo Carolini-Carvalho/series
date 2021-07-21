be0e1269af158edb9d117211e4320ac243d680ab
carolini <acarolini@icloud.com>
2021-07-21 17:07:27 -0300

    configurando auth

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,17 @@ use App\Http\Controllers\TemporadasController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/series', [SeriesController::class,'index'])->name('listar_series');
-Route::get('/series/create', [SeriesController::class,'create'])->name('form_criar_serie');
-Route::post('/series/create', [SeriesController::class,'store']);
-Route::delete('/series/remover/{id}', [SeriesController::class,'destroy']);
-Route::post('/series/{id}/update', [SeriesController::class,'update']);
+Route::middleware(['auth'])->group(function (){
+    Route::get('/series', [SeriesController::class,'index'])->name('listar_series')->middleware('auth');
+    Route::get('/series/create', [SeriesController::class,'create'])->name('form_criar_serie');
+    Route::post('/series/create', [SeriesController::class,'store']);
+    Route::delete('/series/remover/{id}', [SeriesController::class,'destroy']);
+    Route::post('/series/{id}/update', [SeriesController::class,'update']);
 
-Route::get('/series/{serieId}/temporadas', [TemporadasController::class,'index']);
+    Route::get('/series/{serieId}/temporadas', [TemporadasController::class,'index']);
 
-Route::get('/temporadas/{temporada}/episodios', [EpisodiosController::class,'index']);
-
+    Route::get('/temporadas/{temporada}/episodios', [EpisodiosController::class,'index']);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
